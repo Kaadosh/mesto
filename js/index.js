f8e5cb4c6380d0329b0bf24608f60903ff0a9076
Aleksandr Kirillov <kadoshkirillov@yandex.ru>
2023-01-16 13:10:13 +0300

    1. Шесть карточек «из коробки»

--- js/index.js
+++ js/index.js
@@ -34,6 +34,56 @@ popupCloseElement.addEventListener('click', togglePopupVisabillity);
 popupElement.addEventListener('click', closePopupByOnOverley);
 formElement.addEventListener('submit', handleFormSubmit);
 
+// ! Создание карт
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+// ! Добавление карточек через JS по средствам templane
+//  Обращение к templane
+const templateCards = document.querySelector('#cards').content;
+const newCards = document.querySelector('.cards');
+// перебор массива с данными карточкаи методом forEach
+initialCards.forEach(function (element) {
+  const articleCard = templateCards.querySelector('.card').cloneNode(true);
+  articleCard.querySelector('.card__photo').src = element.link;
+  articleCard.querySelector('.card__photo').alt = element.name;
+  articleCard.querySelector('.card__title').textContent = element.name;
+
+  newCards.append(articleCard);
+});
+
+
+
+
+
+
+
+
+
 
 
 
