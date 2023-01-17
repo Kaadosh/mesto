56ed023e6e09df86ce66868b5c6f8eb386410943
Aleksandr Kirillov <kadoshkirillov@yandex.ru>
2023-01-17 14:55:06 +0300

    Форма добавления карточки

--- js/index.js
+++ js/index.js
@@ -1,37 +1,71 @@
-const popupElement = document.querySelector('.popup');
+const popupElement = document.querySelector('.popup_profile');
 const popupCloseElement = popupElement.querySelector('.popup__close');
 const popupEditorElement = document.querySelector('.profile__button');
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__field_input_nickname');
 let jobInput = formElement.querySelector('.popup__field_input_profession');
-
+const openAddCard = document.querySelector('.popup_addCard');
+const closeCard = openAddCard.querySelector('.popup_closeCard');
+const buttonAddCard = document.querySelector('.profile__add');
 let personName = document.querySelector('.profile__title');
 let personJob = document.querySelector('.profile__subtitle');
 
 
+// Открытие профиля и редоктирование
+// const togglePopupVisabillity = function (event) {
+//   popupElement.classList.toggle('popup_opened')
+// }
+// const togglePopupAddCard = function (event) {
+//   openAddCard.classList.toggle('popup_opened')
+// }
+// buttonAddCard.addEventListener('click', function () {
+//   togglePopupAddCard()
+// });
+// !функция открытия профиля
+const openPopupProfile = function () {
+  popupElement.classList.add('popup_opened');
+};
+// ! функция закрытия профиля
+const closePopupProfile = function () {
+  popupElement.classList.remove('popup_opened');
+};
+// ! функция открытия добовления карт
+const openPopupCard = function () {
+  openAddCard.classList.add('popup_opened');
+};
+// ! функция закрытия добовления карт
+const closePopupCard = function () {
+  openAddCard.classList.remove('popup_opened');
+};
 
-const togglePopupVisabillity = function (event) {
-  popupElement.classList.toggle('popup_opened')
-}
 
-const closePopupByOnOverley = function (event) {
-  if (event.target === event.currentTarget) {
-    togglePopupVisabillity();
-  }
-}
+
+
+
+// const closePopupByOnOverley = function (event) {
+//   if (event.target === event.currentTarget) {
+//     togglePopupVisabillity();
+//   }
+// }
 function handleFormSubmit(evt) {
   evt.preventDefault();
   personName.textContent = nameInput.value;
   personJob.textContent = jobInput.value;
-  togglePopupVisabillity()
+  closePopupProfile()
+  // togglePopupVisabillity()
 }
 popupEditorElement.addEventListener('click', function () {
   nameInput.value = personName.textContent;
   jobInput.value = personJob.textContent;
-  togglePopupVisabillity()
+
+  //   togglePopupVisabillity()
 });
-popupCloseElement.addEventListener('click', togglePopupVisabillity);
-popupElement.addEventListener('click', closePopupByOnOverley);
+buttonAddCard.addEventListener('click', openPopupCard);
+closeCard.addEventListener('click', closePopupCard);
+popupEditorElement.addEventListener('click', openPopupProfile,);
+popupCloseElement.addEventListener('click', closePopupProfile);
+// popupCloseElement.addEventListener('click', togglePopupVisabillity);
+// popupElement.addEventListener('click', closePopupByOnOverley);
 formElement.addEventListener('submit', handleFormSubmit);
 
 // ! Создание карт
